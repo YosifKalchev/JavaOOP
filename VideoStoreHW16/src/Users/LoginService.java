5a3014caead304afc80553062e98cbccde51d071
YosifKalchev <ykalchev@icloud.com>
2020-08-12 18:34:07 +0300

    Initial commit

---
+++ VideoStoreHW16/src/Users/LoginService.java
@@ -0,0 +1,9 @@
+package Users;
+
+public interface LoginService {
+
+    String getUsernameFromUser();
+    String getPasswordFromUser();
+    void showAdminOptions();
+    void showUserOptions();
+}
---
+++ VideoStoreHW16/src/Users/User.java
@@ -0,0 +1,15 @@
+package Users;
+
+public class User {
+
+    private String name;
+    private String eMailAdres;
+    private String password;
+
+    public User(String name, String eMailAdres, String password) {
+        this.name = name;
+        this.eMailAdres = eMailAdres;
+        this.password = password;
+    }
+
+}
