d937fe8bbba379a3f4bd5efb2a29828220879744
YosifKalchev <ykalchev@icloud.com>
2020-08-16 16:32:11 +0300

    Added login functionalities

--- VideoStoreHW16/src/Input.java
+++ VideoStoreHW16/src/Input.java
@@ -1,5 +1,10 @@
 public interface Input {
 
+
+
     String getUsernameFromUser();
     String getPasswordFromUser();
+
+    static void showErrorWrongInputNameOrPassword() {
+    }
 }
--- VideoStoreHW16/src/LoginService.java
+++ VideoStoreHW16/src/LoginService.java
@@ -1,15 +1,16 @@
+import DataBase.User;
 import DataBase.UserRepository;
 
 public class LoginService {
 
-    private Input input;
-    private UserRepository userRepository;
+    private  Input input;
+    private  UserRepository userRepository;
+    private User loggedUser;
 
 
     public LoginService (Input input) {
         this.input = input;
         this.userRepository = UserRepository.getInstance();
-        
     }
 
 
@@ -18,7 +19,12 @@ public class LoginService {
          String password = input.getPasswordFromUser();
 
          if (isValid(username) && isValid(password)) {
-            userRepository.getUserByUsername(username);
+             User currentUser = userRepository.getUserByUsername(username);
+             if (currentUser != null && currentUser.getPassword().equals(password)) {
+                 loggedUser = currentUser;
+             } else {
+                 Input.showErrorWrongInputNameOrPassword();
+             }
          }
     }
 
